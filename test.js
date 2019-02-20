e66f7a35d8c114258cde59c5372baac2c612a803
k-sakahara <sakahara.kota@re-kailum.com>
2019-02-20 07:25:46 +0000

    test.jsにTeamクラスを追加した

--- test.js
+++ test.js
@@ -1,3 +1,12 @@
+class Team{
+  constructor(name){
+      this.teamName = name;
+  }
+  getName(){
+    return this.teamname;
+  }
+}
+
 document.addEventListener("click",function(){
   console.log("testing");
   elm3.value = elm1.value + elm2.value
