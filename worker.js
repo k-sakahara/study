8aebe300ad13d0642604e80f2cff6fd17c5305f9
k-sakahara <sakahara.kota@re-kailum.com>
2019-02-22 02:05:39 +0000

    素数計算もっと早くしました。if文一個消しました

--- worker.js
+++ worker.js
@@ -1,9 +1,11 @@
-function isPrime(num){
-  // if(num<2){return false}
-  // else if(num === 2){return true}  //2以下のチェック省略
+const isPrime = function(num){
+  // if(num<2){return false} //2未満は^素数ですけど判定しません
+  // else if(num === 2){return true}  //2は素数ですけど判定しません
   // else
-  if(num %2 == 0){return false};
-  let sqrtNum = Math.sqrt(num);
+
+  // if(num %2 == 0){return false}; //偶数は検証しません 奇数だけ入れてください
+
+  const sqrtNum = Math.sqrt(num);
   for(let i = 3; i<sqrtNum; i+=2){
     if(num%i === 0){
       return false;
@@ -14,6 +16,8 @@ function isPrime(num){
 
 
 self.addEventListener("message",function(e){
+  const startTime = performance.now();
+
   let amo = Number(e.data);
   let count = 0;
   let resultArr = [];
@@ -28,5 +32,8 @@ self.addEventListener("message",function(e){
     }
   }
   // console.log(resultArr);
-  postMessage(resultArr);
+
+  const endTime = performance.now();
+
+  postMessage({result : resultArr, msec : endTime - startTime});
 });
