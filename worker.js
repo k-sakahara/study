613f8ecce3d1d5aa1c2788015f35234134bab87a
k-sakahara <sakahara.kota@re-kailum.com>
2019-02-22 05:12:48 +0000

    とりあえず作ったけど動かない

--- worker.js
+++ worker.js
@@ -1,39 +1,54 @@
-const isPrime = function(num){
-  // if(num<2){return false} //2未満は^素数ですけど判定しません
-  // else if(num === 2){return true}  //2は素数ですけど判定しません
-  // else
-
-  // if(num %2 == 0){return false}; //偶数は検証しません 奇数だけ入れてください
-
-  const sqrtNum = Math.sqrt(num);
-  for(let i = 3; i<sqrtNum; i+=2){
-    if(num%i === 0){
-      return false;
-    }
-  }
-  return true;
-}
-
+const newWorkerViaBlob = function(relativePath) { //localでnew workerできるようになる
+  var baseURL = window.location.href.replace(/\\/g, '/').replace(/\/[^\/]*$/, '/');
+  var array = ['importScripts("' + baseURL + relativePath + '");'];
+  var blob = new Blob(array, {type: 'text/javascript'});
+  var url = window.URL.createObjectURL(blob);
+  return new Worker(url);
+};
 
 self.addEventListener("message",function(e){
   const startTime = performance.now();
 
   let amo = Number(e.data);
-  let count = 0;
   let resultArr = [];
-  resultArr.push(2);
-  count++; //2以外は奇数だけチェックしたいから2が邪魔
 
+  //worker(余り以外)いくつ作るか
+  const workersLength = 4;
+  //何個のworkerから値が返ってきた?
+  let successWorkerCount = 0;
+  //何個のworkerから値が返ってくれば完了?
+  const completeWorkerCount = workersLength+1;
+  //1つのworkerにいくつの値を渡せばよいか?
+  const child_amo = Math.floor(amo/workersLength);
+  //あまりはいくつか
+  const surplus_amo = amo%workersLength;
 
-  for(var i = 3; count < amo; i=i+2){
-    if(isPrime(i)){
-      resultArr.push(i);
-      count++;
+  //child_amoを計算するようのworker生成
+  const workers = [];
+  for(var i = 0; i<workersLength; i++){//4つのworkerを生成する
+    workers.push( newWorkerViaBlob("worker_child.js") );
+    workers[i].postMessage({amo:child_amo,
+                            start:i*child_amo);
+    workers[i].addEventListener("message",function(e){ //child_workerから受け取った時の処理
+      resultArr.push(e.data)
+      successWorkerCount++;
+      if(successWorkerCount === completeWorkerCount){ //すべてのworkerから値が帰ってきた時
+        const endTime = performance.now();
+        postMessage({result : resultArr, msec : endTime - startTime});
+      }
+    });
+  }//endFor
+
+  //surplus_amoを計算するようのworker生成
+  const surplusWorker = newWorkerViaBlob("worker_child.js");
+  surplusWorker.postMessage({amo:surplus_amo,start:workersLength*child_amo+1});
+  surplusWorker.addEventListener("message"){
+    resultArr.push(e.data);
+    successWorkerCount++;
+    if(successWorkerCount === completeWorkerCount){ //すべてのworkerから値が帰ってきた時
+      const endTime = performance.now();
+      postMessage({result : resultArr, msec : endTime - startTime});
     }
   }
-  // console.log(resultArr);
 
-  const endTime = performance.now();
-
-  postMessage({result : resultArr, msec : endTime - startTime});
 });
